f0cc4658c4a0e82add82c3abfff8b7f97c6c4e17
Chris Cannam <cannam@all-day-breakfast.com>
2014-05-30 12:26:53 +0100

    Make the app do something! (If not very much)

--- src/com/breakfastquay/minibpmexample/MiniBpmExampleActivity.java
+++ src/com/breakfastquay/minibpmexample/MiniBpmExampleActivity.java
@@ -12,6 +12,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
 
+import java.nio.ByteBuffer;
+
 import com.breakfastquay.minibpm.MiniBpm;
 
 public class MiniBpmExampleActivity extends Activity
@@ -21,6 +23,48 @@ public class MiniBpmExampleActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+	try {
+	    InputStream audio = getResources().openRawResource(R.raw.a);
+	    ByteArrayOutputStream ostr = new ByteArrayOutputStream(audio.available());
+	    byte[] buf = new byte[1024];
+	    int r;
+	    while ((r = audio.read(buf)) >= 0) ostr.write(buf, 0, r);
+	    byte[] raw = ostr.toByteArray();
+	
+	    // We assume raw is interleaved 2-channel 16-bit PCM
+	    int channels = 2;
+	    int frames = raw.length / (channels * 2);
+	    int rate = 44100;
+
+	    float[] insamples = new float[frames];
+
+	    ByteBuffer bb = ByteBuffer.wrap(raw).order(null);
+
+	    for (int i = 0; i < frames; ++i) {
+		for (int c = 0; c < channels; ++c) {
+		    short val = bb.getShort();
+		    insamples[i] += ((float)val / 32768) / channels;
+		}
+	    }
+
+	    long start = System.currentTimeMillis();
+
+	    MiniBpm mbpm = new MiniBpm(rate);
+	    double tempo = mbpm.estimateTempoOfSamples(insamples, 0, frames);
+	    double[] candidates = mbpm.getTempoCandidates();
+	    mbpm.dispose();
+
+	    long end = System.currentTimeMillis();
+	    double t = (double)(end - start) / 1000;
+
+	    Log.d("MiniBpm", "estimated tempo of " + frames + "-frame stereo track as " + tempo + "bpm in " + t + "s (processing " + (frames / t) + "fps)");
+	    Log.d("MiniBpm", "candidate tempi follow...");
+	    for (int i = 0; i < candidates.length; ++i) {
+		Log.d("MiniBpm", "candidate " + i + ": " + candidates[i]);
+	    }
+
+	} catch (IOException e) { }
 
 	setContentView(R.layout.main);
     }
